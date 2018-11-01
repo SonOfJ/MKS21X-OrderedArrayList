dc28d56a2217611dd2f489ea3d1acd30781a63e7
Jason Zheng <zzheng3@stuy.edu>
2018-11-01 11:47:53 -0400

    trying to figure out what is going on

---
+++ NoNullArrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class NoNullArrayList<T> extends ArrayList<T> {
+  public NoNullArrayList() {
+  }
+  public void set(int index, T element) {
+    try {
+      if (element == null) {
+        throw new 
+      }
+
+    }
+  }
+}
