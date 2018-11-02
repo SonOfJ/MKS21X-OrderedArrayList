c980e45b43260fb17a3c2c26e8d2a0b431e8bb79
Jason Zheng <zzheng3@stuy.edu>
2018-11-02 11:47:54 -0400

    added add and tried implementing constructor with startingCapacity

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
 public class NoNullArrayList<T> extends ArrayList<T> {
-  public NoNullArrayList() {
+  public NoNullArrayList(int startingCapacity) {
+  }
+  public NoNullArrayList(int startingCapacity) {
+    super(startingCapacity);
   }
   public T set(int index, T element) {
     if (element == null) {
