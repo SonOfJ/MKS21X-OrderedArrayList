5ebb755532f9cf1b4ad49b97c246de54a8290824
Jason Zheng <zzheng3@stuy.edu>
2018-11-04 21:48:15 -0500

    added boolean add

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -12,4 +12,11 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     }
     return super.set(index, element);
   }
+  public boolean add(T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Invalid element, can't initiate add command");
+    }
+    super.add(element);
+    return true;
+  }
 }
